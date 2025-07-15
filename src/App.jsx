4278a634fb0049596aa362d7144aa4a5beb55461
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2025-07-15 17:36:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import './index.css';
-import { Routes,Route } from 'react-router-dom'
-import Homepage from './pages/Home'
-import RoomPage from './pages/Room'
-import { SocketProvider } from './providers/Socket'
-import { PeerProvider } from './providers/Peer'
+import React from "react";
+import "./App.css";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Home";
+import RoomPage from "./pages/Room";
+import { SocketProvider } from "./providers/Socket";
+import { PeerProvider } from "./providers/Peer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    
-      <div className="App">
-        <SocketProvider>
-          <PeerProvider>
-          <Routes>
-          
-            <Route path='/' element = {<Homepage/>} />
-            <Route path='/room/:roomId' element={<RoomPage/>} />
-          </Routes>
-          </PeerProvider>
-        </SocketProvider>
-      </div>
-    </>
-  )
+    return (
+        <div className="App">
+            <SocketProvider>
+                <PeerProvider>
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/room/:roomId" element={<RoomPage />} />
+                    </Routes>
+                </PeerProvider>
+            </SocketProvider>
+        </div>
+    );
 }
 
-export default App
+export default App;
